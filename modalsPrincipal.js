0327b962ac6738bbf5e5c4b32d987226b80c05ac
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2024-12-05 22:06:07 -0300

    AdicionadasTodasTerminologiasParaoMPrincipal.js-FaltandoOModals.js

--- modalsPrincipal.js
+++ modalsPrincipal.js
@@ -9,6 +9,17 @@ var alcanosM = document.getElementById("alcanosM");
 var aldeidosM = document.getElementById("aldeidosM");
 var acidosM = document.getElementById("acidosM");
 var balancaM = document.getElementById("balancaM");
+var carbonilaM = document.getElementById("carbonilaM");
+var catalisadorM = document.getElementById("catalisadorM");
+var cloroformioM = document.getElementById("cloroformioM");
+var compostoM = document.getElementById("compostoM");
+var corrosaoM = document.getElementById("corrosaoM");
+var cetonasM = document.getElementById("cetonasM");
+var digestorM = document.getElementById("digestorM");
+var etilenoglicolM = document.getElementById("etilenoglicolM");
+var surfatantesM = document.getElementById("surfatantesM");
+var turbidimetriaM = document.getElementById("turbidimetriaM");
+
 // Get the button that opens the modal
 var Atomo = document.getElementById("Atomo");
 var bicarbonatos = document.getElementById("bicarbonatos");
@@ -20,6 +31,16 @@ var alcanos = document.getElementById("alcanos");
 var aldeidos = document.getElementById("aldeidos");
 var acidos = document.getElementById("acidos");
 var balanca = document.getElementById("balanca");
+var carbonila = document.getElementById("carbonila");
+var catalisador = document.getElementById("catalisador");
+var cloroformio = document.getElementById("cloroformio");
+var composto = document.getElementById("composto");
+var corrosao = document.getElementById("corrosao");
+var cetonas = document.getElementById("cetonas");
+var digestor = document.getElementById("digestor");
+var etilenoglicol = document.getElementById("etilenoglicol");
+var surfatantes = document.getElementById("surfatantes");
+var turbidimetria = document.getElementById("turbidimetria");
 
 // Get the <span> element that closes the modal
 var close = document.getElementsByClassName("close")[0];
@@ -32,6 +53,16 @@ var close7 = document.getElementsByClassName("close")[6];
 var close8 = document.getElementsByClassName("close")[7];
 var close9 = document.getElementsByClassName("close")[8];
 var close10 = document.getElementsByClassName("close")[9];
+var close11 = document.getElementsByClassName("close")[10];
+var close12 = document.getElementsByClassName("close")[11];
+var close13 = document.getElementsByClassName("close")[12];
+var close14 = document.getElementsByClassName("close")[13];
+var close15 = document.getElementsByClassName("close")[14];
+var close16 = document.getElementsByClassName("close")[15];
+var close17 = document.getElementsByClassName("close")[16];
+var close18 = document.getElementsByClassName("close")[17];
+var close19 = document.getElementsByClassName("close")[18];
+var close20 = document.getElementsByClassName("close")[19];
 
 // When the user clicks the button, open the modal 
 Atomo.onclick = function() {
@@ -114,6 +145,87 @@ balanca.onclick = function() {
     balancaM.style.display = "block";
   }
 }
+carbonila.onclick = function() {
+  if (carbonilaM.style.display == "block") {
+    carbonilaM.style.display = "none";
+  }
+  else if (carbonilaM.style.display == "none" || carbonilaM.style.display == "") {
+    carbonilaM.style.display = "block";
+  }
+}
+catalisador.onclick = function() {
+  if (catalisadorM.style.display == "block") {
+    catalisadorM.style.display = "none";
+  }
+  else if (catalisadorM.style.display == "none" || catalisadorM.style.display == "") {
+    catalisadorM.style.display = "block";
+  }
+}
+cloroformio.onclick = function() {
+  if (cloroformioM.style.display == "block") {
+    cloroformioM.style.display = "none";
+  }
+  else if (cloroformioM.style.display == "none" || cloroformioM.style.display == "") {
+    cloroformioM.style.display = "block";
+  }
+}
+composto.onclick = function() {
+  if (compostoM.style.display == "block") {
+    compostoM.style.display = "none";
+  }
+  else if (compostoM.style.display == "none" || compostoM.style.display == "") {
+    compostoM.style.display = "block";
+  }
+}
+corrosao.onclick = function() {
+  if (corrosaoM.style.display == "block") {
+    corrosaoM.style.display = "none";
+  }
+  else if (corrosaoM.style.display == "none" || corrosaoM.style.display == "") {
+    corrosaoM.style.display = "block";
+  }
+}
+cetonas.onclick = function() {
+  if (cetonasM.style.display == "block") {
+    cetonasM.style.display = "none";
+  }
+  else if (cetonasM.style.display == "none" || cetonasM.style.display == "") {
+    cetonasM.style.display = "block";
+  }
+}
+digestor.onclick = function() {
+  if (digestorM.style.display == "block") {
+    digestorM.style.display = "none";
+  }
+  else if (digestorM.style.display == "none" || digestorM.style.display == "") {
+    digestorM.style.display = "block";
+  }
+}
+etilenoglicol.onclick = function() {
+  if (etilenoglicolM.style.display == "block") {
+    etilenoglicolM.style.display = "none";
+  }
+  else if (etilenoglicolM.style.display == "none" || etilenoglicolM.style.display == "") {
+    etilenoglicolM.style.display = "block";
+  }
+}
+surfatantes.onclick = function() {
+  if (surfatantesM.style.display == "block") {
+    surfatantesM.style.display = "none";
+  }
+  else if (surfatantesM.style.display == "none" || surfatantesM.style.display == "") {
+    surfatantesM.style.display = "block";
+  }
+}
+turbidimetria.onclick = function() {
+  if (turbidimetriaM.style.display == "block") {
+    turbidimetriaM.style.display = "none";
+  }
+  else if (turbidimetriaM.style.display == "none" || turbidimetriaM.style.display == "") {
+    turbidimetriaM.style.display = "block";
+  }
+}
+
 // When the user clicks on <span> (x), close the modal
 function closeAll(){
   AtomoM.style.display = "none";
@@ -126,6 +238,17 @@ function closeAll(){
   aldeidosM.style.display = "none";
   acidosM.style.display = "none";
   balancaM.style.display = "none";
+  carbonilaM.style.display = "none";
+  catalisadorM.style.display = "none";
+  cloroformioM.style.display = "none";
+  compostoM.style.display = "none";
+  corrosaoM.style.display = "none";
+  cetonasM.style.display = "none";
+  digestorM.style.display = "none";
+  etilenoglicolM.style.display = "none";
+  surfatantesM.style.display = "none";
+  turbidimetriaM.style.display = "none";
+
 }
 close.onclick = function(){
   closeAll();
@@ -155,6 +278,36 @@ close9.onclick = function(){
   closeAll();
 }
 close10.onclick = function(){
+  closeAll();
+}
+close11.onclick = function(){
+  closeAll();
+}
+close12.onclick = function(){
+  closeAll();
+}
+close13.onclick = function(){
+  closeAll();
+}
+close14.onclick = function(){
+  closeAll();
+}
+close15.onclick = function(){
+  closeAll();
+}
+close16.onclick = function(){
+  closeAll();
+}
+close17.onclick = function(){
+  closeAll();
+}
+close18.onclick = function(){
+  closeAll();
+}
+close19.onclick = function(){
+  closeAll();
+}
+close20.onclick = function(){
   closeAll();
 }
 
@@ -190,5 +343,53 @@ window.onclick = function(event) {
   else if (event.target == balancaM) {
     balancaM.style.display = "none";
   }
-}//A balança de precisão permite determinar a massa de alguns poucos miligramas.
+  else if (event.target == carbonilaM) {
+    carbonilaM.style.display = "none";
+  }
+  else if (event.target == catalisadorM) {
+    catalisadorM.style.display = "none";
+  }
+  else if (event.target == cloroformioM) {
+    cloroformioM.style.display = "none";
+  }
+  else if (event.target == compostoM) {
+    compostoM.style.display = "none";
+  }
+  else if (event.target == corrosaoM) {
+    corrosaoM.style.display = "none";
+  }
+  else if (event.target == cetonasM) {
+    cetonasM.style.display = "none";
+  }
+  else if (event.target == digestorM) {
+    digestorM.style.display = "none";
+  }
+  else if (event.target == etilenoglicolM) {
+    etilenoglicolM.style.display = "none";
+  }
+  else if (event.target == surfatantesM) {
+    surfatantesM.style.display = "none";
+  }
+  else if (event.target == turbidimetriaM) {
+    turbidimetriaM.style.display = "none";
+  }
+}
+//cetonas digestor etilenoglicol surfatantes turbidimetria //Add para o html.
+
+//Cetonas
+//As cetonas , por sua vez, são compostos orgânicos que apresentam o grupo carbonila no meio da cadeia carbônica, ligado a dois grupos orgânicos. </s><s> Fórmula geral das cetonas R1 – CO – R2 R1 – e – R2 são grupos orgânicos que podem ser diferentes ou idênticos (como no caso da acetona). </s><s> Já no caso das cetonas, o sufixo utilizado é ona. </s><s> O nome químico da acetona, segundo a mesma nomenclatura, é propanona, em que o prefixo prop 4 3 2 1 CH3 CH CH CHO indica que a cadeia do composto apresenta três átomos de carbono.  Já nas cetonas é necessário indicar a posição da carbonila na cadeia carbônica, que pode variar. </s><s> Isso não ocorre no caso 1 5 4 3 2 CH3 CH CH CO CH3 da acetona (propanona), como vimos, porque a posição da carbonila, numa cetona com três átomos de carbono, só pode ser CH3 CH3 a posição 2. </s><s> Se a carbonila estivesse na posição 1, o composto Figura 1.53 seria um aldeído e não uma cetona, e seu nome seria propanal.
+
+//Digestor
+//Equipamento de agitação para determinação do índice de preparo com 2 ou 4 provas, regulado para girar a 50 - 60 rpm
+
+//etileno-glicol
+//1,2-etanodiol
+
+//Surfatantes
+//Os surfatantes são constituídos por uma parte hidrófila, geralmente iônica, e uma parte lipofílica formada por hidrocarbonetos com oito a dezoito átomos de carbono, linear ou ligeiramente ramificada, contendo, na maioria dos produtos comercializados, um anel benzênico substituindo alguns átomos de cadeia. 
+
+//Turbidimetria
+//quantificação em espectrofotômetro ou colorímetro a 660 nm, uma vez que neste comprimento de onda, a cor geralmente parda dos meios de cultura não interfere com os resultados
+
+
 //The same prompt i've used to create the last file, but with this text "" inside the <h2> tag.
